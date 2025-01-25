3c7300a3f03107a9bde13106fd771a9075c99477
Nantege Jane <janepounds1@gmail.com>
2025-01-25 21:44:35 +0300

    added create account screen

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -1,22 +1,296 @@
 import 'package:flutter/material.dart';
 
-class CreateAccountScreen extends StatelessWidget {
+class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
+
+  @override
+  State<CreateAccountScreen> createState() => _CreateAccountScreenState();
+}
+
+class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  bool _isChecked = false; // To manage the checkbox state
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create Account')),
-      body: Form(
+      body: SafeArea(
         child: Column(
           children: [
-            TextField(decoration: InputDecoration(labelText: 'Name')),
-            TextField(decoration: InputDecoration(labelText: 'Email')),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/main'),
-              child: Text('Submit'),
+            // Main Content Area with Scroll
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Header Section
+                   Container(
+      width: double.infinity,
+      color: const Color(0xFFA78A52), // Header background color
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Create Account',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                'Enter your details',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const Icon(
+            Icons.menu, // Example menu icon
+            color: Colors.white,
+            size: 30,
+          ),
+        ],
+      ),
+    ),
+
+                    // Form Section
+                     Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Colors.white, // Form background color
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30), // Curve only the top-right corner
+        ),
+      ),
+      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          _buildTextField('First Name'),
+                          const SizedBox(height: 16),
+                          _buildTextField('Last Name'),
+                          const SizedBox(height: 16),
+                          _buildTextField('Email'),
+                          const SizedBox(height: 16),
+
+                          // Phone Number Field
+                          Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/images/south_africa.png',
+                                      width: 24,
+                                      height: 24,
+                                    ),
+                                    const SizedBox(width: 8),
+                                    const Text('+27', style: TextStyle(fontSize: 16)),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Expanded(child: _buildTextField('Phone Number')),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          _buildTextField('Password', obscureText: true),
+                          const SizedBox(height: 16),
+                          _buildTextField('Confirm Password', obscureText: true),
+                          const SizedBox(height: 16),
+
+                          // Terms and Conditions
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Checkbox(
+                                value: _isChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isChecked = value!;
+                                  });
+                                },
+                              ),
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // Navigate to Terms and Conditions
+                                  },
+                                  child: const Text.rich(
+                                    TextSpan(
+                                      text: 'I agree to the ',
+                                      style: TextStyle(color: Colors.black),
+                                      children: [
+                                        TextSpan(
+                                          text: 'Terms and Conditions',
+                                          style: TextStyle(
+                                            color: Colors.blue,
+                                            decoration: TextDecoration.underline,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 24),
+
+                          // Sign Up Button
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle sign-up logic
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFA78A52),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 12,
+                              ),
+                            ),
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+
+                          // Already Have an Account? Sign In
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Already have an account? ',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  // Navigate to Sign In screen
+                                },
+                                child: const Text(
+                                  'Sign In!',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Footer Section
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFFF4F4F4), // Footer background color
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    '©Otela',
+                    style: TextStyle(fontSize: 12, color: Colors.black),
+                  ),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to Privacy
+                        },
+                        child: const Text(
+                          'Privacy',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to Legal
+                        },
+                        child: const Text(
+                          'Legal',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to Contact
+                        },
+                        child: const Text(
+                          'Contact',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  // Helper to create text fields
+  Widget _buildTextField(String hintText, {bool obscureText = false}) {
+    return TextFormField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.grey),
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide.none,
         ),
       ),
     );
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:otela_investment_club_app/screens/create_account.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,18 +7,155 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/createAccount'),
-            child: Text('Create Account'),
+      backgroundColor: Color(0xFFDCB765), // Background color
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo at the top
+              Stack(
+                clipBehavior: Clip.none, // Allow the image to overflow if necessary
+                alignment: Alignment.topCenter,
+                children: [
+                  // "otela" text
+                  Text(
+                    'otela',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                  ),
+                  // Image positioned at the top-right of "otela"
+                  Positioned(
+                    top: -30, // Adjust this value as needed
+                    right: -40, // Adjust this value as needed
+                    child: Image.asset(
+                      'assets/images/logo_no_text.png', // Replace with your image path
+                      width: 50, // Adjust the size of the image
+                      height: 50,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              // Login form
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+             TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                      hintStyle: TextStyle(
+                      color: Colors.white, // Change to the desired hint text color
+                      fontSize: 16,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFa78a52),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none, // No border when the field is not focused
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none, // No border when the field is focused
+                    ),
+                  ),
+                ),
+
+                  SizedBox(height: 16),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.white, // Change to the desired hint text color
+                          fontSize: 16,
+                        ),
+                      filled: true,
+                      fillColor: Color(0xFFa78a52),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                       enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none, // No border when the field is not focused
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none, // No border when the field is focused
+                    ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add navigation logic
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFF9E0B1),
+                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                        ),
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFA78A52),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to Registration Screen
+                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => CreateAccountScreen()),
+                                      );
+
+                        },
+                        child: Text(
+                          'Register Here',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // Terms & Conditions
+              SizedBox(height: 30),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RichText(
+                  text: TextSpan(
+                    text: 'By signing in, you agree to our ',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Terms & Conditions.',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/main'),
-            child: Text('Log In'),
-          ),
-        ],
+        ),
       ),
     );
   }
